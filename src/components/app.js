d53b763d63011ce7f6326dfe78c8d107f7a02925
Glatz <zsglatz@gmail.com>
2018-03-27 15:26:21 -0600

    Added Reactstrap and inputs

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Greeting from './madlib_form';
-import madlib_form from './madlib_form';
+import MadlibForm from './madlib_form';
 
 export default class App extends Component {
   render() {
@@ -13,7 +12,7 @@ export default class App extends Component {
             Fill out the fields below and click the generate button <br/> to see the madlib story
             </div>
          </div>
-         <Greeting name="Zayne"/>
+         <MadlibForm/>
       </div>
     </div>
     );
--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import {
+    Col, 
+    Row,
+    Label
+} from 'reactstrap';
 
-class Greeting extends React.Component {
+class MadlibForm extends React.Component {
+
+    handleChange = function() {
+        console.log('Trying to handle change');
+    }
+    
+    
     render() {
-        return <h1> Hello, {this.props.name} </h1>
+        return (
+            <Row style={{textAlign: 'center', color: 'white'}}> 
+                <Col md="3"> 
+                    <Row>
+                        <Col md="2">
+                            <label className="greenLabel">1</label>
+                        </Col>
+                        <Col md="10">
+                            <input placeholder="Color" type='text' onChange={this.handleChange} />
+                        </Col>
+                    </Row>
+                 </Col>
+            <Row>
+                <Col md="2">
+                    <label className="greenLabel">1</label>
+                </Col>
+                <Col md="10">
+                    <input placeholder="Color" type='text'/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md="2">
+                    <label className="greenLabel">1</label>
+                </Col>
+                <Col md="10">
+                    <input placeholder="Color" type='text'/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md="2">
+                    <label className="greenLabel">1</label>
+                </Col>
+                <Col md="10">
+                    <input placeholder="Color" type='text'/>
+                </Col>
+            </Row>
+        </Row>
+        )
     }
 }
 
-export default Greeting;
+export default MadlibForm;
